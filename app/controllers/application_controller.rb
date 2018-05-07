4ba6da97fe072d712ee0155dac75108a34f16d24
Brandy Austin <brandy.austin.seattle@gmail.com>
2018-05-07 11:29:27 -0700

    Edited application_controller.rb to have :null_sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  # null_session instead of :exception bc we don't have an authenticity token and rails expects us to always have a token when giving/receiving data
+  protect_from_forgery with: :null_session
 end
